8758a9aa903046ff6696640b3213001beb504186
jamesterzzzzzz <jg@xmpro.com>
2024-07-09 00:07:06 +0800

    created servuces | endpoints | manage hosting | handlers

---
+++ Extensions/HostingExtension.cs
@@ -0,0 +1,63 @@
+﻿using SolNet.Services;
+using solnet_project_demo.Endpoints;
+using solnet_project_demo.Services;
+
+namespace solnet_project_demo.Extensions;
+
+public static class HostingExtension
+{
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        // Add services to the container.
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddScoped<ISolnetService, SolNetService>();
+
+        return builder.Build();
+    }
+
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        app.UseHttpsRedirection();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.MapGroup("/solana")
+            .SolanaGroupEndpoints();
+
+        var summaries = new[]
+        {
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+        app.MapGet("/weatherforecast", () =>
+        {
+            var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+                .ToArray();
+            return forecast;
+        })
+        .WithName("GetWeatherForecast")
+        .WithOpenApi();
+
+        return app;
+    }
+
+    record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+    {
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+
+}
